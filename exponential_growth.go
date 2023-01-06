46b3fafaf6050c7773c574c8c53ea377b3266c9b
fdoudou <softgeekvert@gmail.com>
2023-01-06 13:30:54 +0000

    Creating a simple go program for testing purpose

--- exponential_growth.go
+++ exponential_growth.go
@@ -8,25 +8,7 @@ func main() {
 	numRabbits := 0
 	//your code goes here
 	for c := 1; c <= years; c++ {
-		numRabbits += numRabbits + c*2
+		numRabbits += numRabbits * 2
 	}
 	fmt.Println(numRabbits)
 }
-
-/*
-package main
-
-import "fmt"
-
-func main() {
-    var years int
-    fmt.Scanln(&years)
-    numRabbits := 7
-    //your code goes here
-    for c :=0;c<years;c++ {
-        numRabbits += numRabbits + c * 2
-    }
-    fmt.Println(numRabbits)
-}
-
-*/
